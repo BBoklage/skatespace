0e5cbf597eadda761a841f705cac2c0fa51aaf37
blaine <bboklage@live.com>
2019-11-16 10:58:00 -0700

    Navbar styling done and responsive

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,14 @@
+
 require('./bootstrap');
 
+// console.log('working');
+window.Vue = require('vue');
 
 // vue components 
 Vue.component('vidshare', require('./components/VideoShare.vue').default);
+
+
+// new vue instance 
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+    <div class="row">
+        <!-- left side  -->
+        <div class="col-md-6">
+            <vidshare></vidshare>
+
+        </div>
+        <div class="col-md-6">
+            
+
+        </div>
+
+
+    </div>
+
+
+
+@endsection
